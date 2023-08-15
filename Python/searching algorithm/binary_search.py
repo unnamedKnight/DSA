8a8f611bce87081a1dc8c3f4e86320aa048c3350
Wanderer <backfrontdev47@gmail.com>
2023-08-15 15:24:16 +0600

    Another implementaiotn of binary search

--- Python/searching algorithm/binary_search.py
+++ Python/searching algorithm/binary_search.py
@@ -5,19 +5,17 @@ def binary_search(sorted_li: list[int], target: int) -> int:
     head_pointer: int = sorted_li.index(sorted_li[0])
     tail_pointer: int = sorted_li.index(sorted_li[-1])
     middle_pointer: int = floor((head_pointer + tail_pointer) / 2)
-    # print(middle_pointer)
     found: bool = False
     cycle_count: int = 0
 
-    print('Cycle Starts')
     while not found:
         cycle_count += 1
-        print(f"Cycle Count: {cycle_count}")
-        print(f'tail_pointer*******: {tail_pointer}')
-        print(f'head pointer: {sorted_li[head_pointer]}')
-        print(f'middle pointer: {sorted_li[middle_pointer]}')
-        print(f'tail pointer: {sorted_li[tail_pointer]}')
-        print(f'target: {target}')
+        # print(f"Cycle Count: {cycle_count}")
+        # print(f'tail_pointer*******: {tail_pointer}')
+        # print(f'head pointer: {sorted_li[head_pointer]}')
+        # print(f'middle pointer: {sorted_li[middle_pointer]}')
+        # print(f'tail pointer: {sorted_li[tail_pointer]}')
+        # print(f'target: {target}')
         if target == sorted_li[middle_pointer]:
             return target
 
