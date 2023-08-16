7cc541d2abc58e20eb0dc71ea38fbf5ff79d7cb0
Wanderer <backfrontdev47@gmail.com>
2023-08-16 10:33:29 +0600

    implementation of Insertion sort

--- Python/sorting algorithm/insertion_sort.py
+++ Python/sorting algorithm/insertion_sort.py
@@ -1,7 +1,7 @@
 def insertion_sort(arr: list[int]) -> list[int]:
     for i in range(1, len(arr)):
         key = arr[i]
-        last = i - 1
+        last = i - 1   # getting index value
         while last >= 0 and key < arr[last]:
             arr[last + 1] = arr[last]
             last -= 1
