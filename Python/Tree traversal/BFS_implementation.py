465cdcc7cc06e2185f3e94f7e0f55495dbb228aa
Wanderer <backfrontdev47@gmail.com>
2023-08-23 10:36:35 +0600

    Implementation of BFS

--- Python/Tree traversal/BFS_implementation.py
+++ Python/Tree traversal/BFS_implementation.py
@@ -1,3 +1,27 @@
+"""
+Implementation of DFS
+"""
+
+
+class Queue:
+    def __init__(self) -> None:
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def deque(self):
+        if len(self.items):
+            return self.items.pop(0)
+
+    def peek(self):
+        if len(self.items):
+            return self.items[0].value
+
+    def __len__(self):
+        return len(self.items)
+
+
 class Node:
     def __init__(self, value) -> None:
         self.value = value
@@ -5,14 +29,29 @@ class Node:
         self.right = None
 
 
-'''
-Implementation of DFS
-'''
-
 class Tree:
     def __init__(self, value) -> None:
         self.root = Node(value)
 
+    def level_order(self, start):
+        if start is None:
+            return
+
+        queue = Queue()
+        queue.enqueue(start)
+        traversal = []
+
+        while len(queue) > 0:
+            traversal.append(queue.peek())
+            node = queue.deque()
+
+            if node.left:
+                queue.enqueue(node.left)
+
+            if node.right:
+                queue.enqueue(node.right)
+
+        return traversal
 
 
 
@@ -25,3 +64,5 @@ tree.root.left.right = Node(7)
 
 tree.root.right.left = Node(8)
 tree.root.right.right = Node(9)
+
+print(tree.level_order(tree.root))
